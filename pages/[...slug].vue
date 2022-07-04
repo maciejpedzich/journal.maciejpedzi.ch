80cf1c7e57ee1c8dd4b08732864b175c386ba856
maciejpedzich <contact@maciejpedzi.ch>
2022-07-04 12:03:32 +0200

    Render title and description separately from body

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,9 +1,22 @@
 <template>
   <article>
     <ContentDoc>
+      <template #default="{ doc }">
+        <h1>{{ doc.title }}</h1>
+        <p id="desc">
+          <em>{{ doc.description }}</em>
+        </p>
+        <ContentRenderer :value="doc" />
+      </template>
       <template #not-found>
         <p>This page doesn't exist!</p>
       </template>
     </ContentDoc>
   </article>
 </template>
+
+<style scoped>
+#desc {
+  text-align: center;
+}
+</style>
