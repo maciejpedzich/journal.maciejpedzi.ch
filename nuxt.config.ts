6a9edb2c674190aa4a72bce485fd819de7d697ec
maciejpedzich <contact@maciejpedzi.ch>
2022-07-05 16:28:12 +0200

    Install and configure social tags module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,20 @@ import eslintPlugin from 'vite-plugin-eslint';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ['~~/assets/styles.css'],
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', 'nuxt-social-tags'],
   content: {
     highlight: {
       theme: 'nord'
     }
+  },
+  socialtags: {
+    enabled: true,
+    opengraph: true,
+    twitter: true,
+    img: 'https://journal.maciejpedzi.ch/images/banner.png',
+    theme_color: '#485870',
+    twitter_user: 'MaciejPedzich',
+    twitter_card: 'summary_large_image'
   },
   nitro: {
     prerender: {
