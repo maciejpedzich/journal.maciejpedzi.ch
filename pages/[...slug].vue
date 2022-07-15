b2976e87d93f15165b70cbb970122791f83379d5
maciejpedzich <contact@maciejpedzi.ch>
2022-07-15 12:06:58 +0200

    Display article's publishing date below the title

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,8 +1,28 @@
+<script setup lang="ts">
+const dateFrags = (doc) =>
+  new Intl.DateTimeFormat('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  })
+    .formatToParts(new Date(doc.date))
+    .filter(({ type }) => type !== 'literal')
+    .map((frag) => {
+      frag.value = frag.value.toLowerCase();
+      return frag;
+    });
+</script>
+
 <template>
   <article>
     <ContentDoc>
       <template #default="{ doc }">
         <h1>{{ doc.title }}</h1>
+        <p id="date-published" class="cmr2-text">
+          <span v-for="{ type, value } of dateFrags(doc)" :key="type">
+            {{ value }}
+          </span>
+        </p>
         <p id="desc">
           <em>{{ doc.description }}</em>
         </p>
@@ -18,5 +38,9 @@
 <style scoped>
 #desc {
   text-align: center;
+}
+
+#date-published {
+  margin-top: 0;
 }
 </style>
